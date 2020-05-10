798a3b038fde3f1afdd784756467f5afb8d731ae
nomen1 <olesiamakarenko5115@gmail.com>
2020-05-10 13:09:20 +0300

    Updates

---
+++ precache-manifest.9ccefe314819198f87d0beef685c3661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b860d160ff1e91015eb073ebfdeb894",
+    "url": "/nomen1/joke-tz/index.html"
+  },
+  {
+    "revision": "4e41c3930359b579b94a",
+    "url": "/nomen1/joke-tz/static/css/main.f536576d.chunk.css"
+  },
+  {
+    "revision": "3e7eb3e6567b684ce09b",
+    "url": "/nomen1/joke-tz/static/js/2.a791ee25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nomen1/joke-tz/static/js/2.a791ee25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e41c3930359b579b94a",
+    "url": "/nomen1/joke-tz/static/js/main.8ddea153.chunk.js"
+  },
+  {
+    "revision": "148289e9a798446e7bfc",
+    "url": "/nomen1/joke-tz/static/js/runtime-main.6f298f69.js"
+  }
+]);
